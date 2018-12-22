ae3be963addacd88efdb1ccfa326c3b9e6c96e36
Carlos <pardtheman@gmail.com>
2018-12-22 12:37:11 +0100

    #time 30m #comment models with migrations

--- mastermind/models.py
+++ mastermind/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.db import models
     
 class Peg(models.Model):
-    
+
+    BLACK = 'black'
+    WHITE = 'white'  
     RED = '#FF0000'
     BLUE = '#0000FF'
     GREEN = '#00FF00'
     YELLOW = '#FFFF00'
     COLORS = (
+        (BLACK, '#000'),
+        (WHITE, '#FFF'),
         (RED, 'red'),
         (BLUE, 'blue'),
         (GREEN, 'green'),
@@ -17,27 +21,55 @@ class Peg(models.Model):
     color = models.CharField(max_length=10, choices=COLORS)
     position = models.IntegerField(default=0)
 
-class Code(models.Model):
+class FeedbackPeg(Peg):
+	pass
 
-    BLACK = 'black'
-    WHITE = 'white'
-    KEY_PEGS = (
-        (BLACK, '#000'),
-        (WHITE, '#FFF'),
-    )
+class GuessPeg(Peg):
+
+    def save(self, *args, **kwargs):
+        self.color = self.RED
+        super().save(*args, **kwargs)
+
+
+class Code(models.Model):
 
     pegs = models.ManyToManyField(Peg)
 
     def compare_code(self, other_code):
-    	# TODO
-    	pass
+        # TODO
+        pass
 
 
 class Game(models.Model):
 
+    EASY = 3
+    MEDIUM = 4
+    HARD = 5
+    INSANE = 101
+    DIFFICULTY = (
+        (EASY, 'Fácil'),
+        (MEDIUM, 'Medio'),
+        (HARD, 'Difícil'),
+        (INSANE, 'Muy difícil'),
+    )
+
     username = models.CharField(max_length=50)
+    difficulty = models.IntegerField(default=MEDIUM)
     secret_code = models.ForeignKey(Code, related_name='games', on_delete=models.CASCADE)
 
+
+    def save(self, *args, **kwargs):
+        self.secret_code = self.generate_random_code()
+        super().save(*args, **kwargs)
+
     def get_historical(self):
-    	# TODO
-    	pass
+        # TODO
+        pass
+
+    def generate_random_code(self):
+        guess_pegs = []
+        for i in range(self.difficulty):
+        	guess_pegs.append(GuessPeg.objects.create())
+        code = Code.objects.create()
+        code.pegs.set(guess_pegs)
+        return code
